9c41c31c1c60034044c0e09f34ea18bad5391a76
Kyle Bloom <kylejbloom@gmail.com>
2023-08-06 21:29:56 +0100

    Remove extra stuff on event emitter

--- typings/react-native-beacons-manager.d.ts
+++ typings/react-native-beacons-manager.d.ts
@@ -1,26 +1,8 @@
-class TypedEventEmitter<TEvents extends Record<string, any>> {
-  private emitter = new EventEmitter()
-
-  emit<TEventName extends keyof TEvents & string>(
-    eventName: TEventName,
-    ...eventArg: TEvents[TEventName]
-  ) {
-    this.emitter.emit(eventName, ...(eventArg as []))
-  }
-
-  on<TEventName extends keyof TEvents & string>(
+interface TypedEventEmitter<TEvents extends Record<string, any>> {
+  addListener<TEventName extends keyof TEvents & string>(
     eventName: TEventName,
     handler: (...eventArg: TEvents[TEventName]) => void
-  ) {
-    this.emitter.on(eventName, handler as any)
-  }
-
-  off<TEventName extends keyof TEvents & string>(
-    eventName: TEventName,
-    handler: (...eventArg: TEvents[TEventName]) => void
-  ) {
-    this.emitter.off(eventName, handler as any)
-  }
+  )
 }
 
 declare module '@drive-thru-dark-rides/react-native-beacons-manager' {
